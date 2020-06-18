93a6f89d27b8b8d67c1c031690968e2ca363214b
narith-minami <narith.for.work@gmail.com>
2020-06-18 10:30:10 +0900

    add button (test)

---
+++ components/Button.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  button.button(@click="onClick") ボタン
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick() {
+      alert("click!!");
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.button
+  width: 300px
+</style>
